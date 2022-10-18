cd62bfc7d20dd611e8ac3ee9578380b383276c60
Mike Price <mprice@healthwise.org>
2022-10-17 19:18:30 -0600

    Clean up and added solution items to make them easier to update using visual studio

--- Healthwise.Sdo.Functions/ProcessEvent.cs
+++ Healthwise.Sdo.Functions/ProcessEvent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using System.Threading.Tasks;
 using Azure.Messaging.EventHubs;
 using Azure.Storage;
